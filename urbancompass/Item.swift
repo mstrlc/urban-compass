0d000468ef03921df1009a9293ab094e3f201939
Matyáš Strelec <matyasstrelec@gmail.com>
2025-04-23 19:40:23 +0200

    Initial Commit

---
+++ urbancompass/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  urbancompass
+//
+//  Created by Matyáš Strelec on 23.04.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ urbancompassTests/urbancompassTests.swift
@@ -0,0 +1,17 @@
+//
+//  urbancompassTests.swift
+//  urbancompassTests
+//
+//  Created by Matyáš Strelec on 23.04.2025.
+//
+
+import Testing
+@testable import urbancompass
+
+struct urbancompassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
